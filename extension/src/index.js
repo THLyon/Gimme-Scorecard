cb3129701c452a4aacb896e4423865cd9a70da71
Tanner <tannerhlyon@gmail.com>
2023-12-30 14:22:56 -0800

    updated logic for alternating div color scheme; fixed scroll (vertical, horizontal) issues; fixed spacing and alignment of inner html text; TODO: fix CORS issue to allow loading of secured html, currently loading http; add aws server for publish; eventually, add customization, db and account verification

--- extension/src/index.js
+++ extension/src/index.js
@@ -2,22 +2,46 @@
 document.addEventListener('DOMContentLoaded', () => {
     getScoreCard();
 
+    let count = 0; 
+
     const leaderboardList = document.querySelector('#leaderboard-list');
 
     const makeListItem = (pos, name, rank, totalScore) => {
+
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('data-row'); // Use the 'data-row' class for styling
+
+        if (count % 2 === 0) {
+            rowContainer.style.backgroundColor = '#6BA155'; // Change to your specific green color
+        } else {
+            rowContainer.style.backgroundColor = '#D4D4D2'; // Change to your specific gray color
+        }
     
+        // const positionDiv = document.createElement('div');
+        // positionDiv.innerHTML = `<strong>Position:</strong> ${pos + 1}`;
+    
+        // const nameDiv = document.createElement('div');
+        // nameDiv.innerHTML = `<strong>Name:</strong> ${name}`;
+    
+        // const rankDiv = document.createElement('div');
+        // rankDiv.innerHTML = `<strong>Rank:</strong> ${rank}`;
+    
+        // const totalScoreDiv = document.createElement('div');
+        // totalScoreDiv.innerHTML = `<strong>Total Score:</strong> ${totalScore}`;
         const positionDiv = document.createElement('div');
+        positionDiv.classList.add('position-div');
         positionDiv.innerHTML = `<strong>Position:</strong> ${pos + 1}`;
-    
+
         const nameDiv = document.createElement('div');
+        nameDiv.classList.add('name-div');
         nameDiv.innerHTML = `<strong>Name:</strong> ${name}`;
-    
+
         const rankDiv = document.createElement('div');
+        rankDiv.classList.add('rank-div');
         rankDiv.innerHTML = `<strong>Rank:</strong> ${rank}`;
-    
+
         const totalScoreDiv = document.createElement('div');
+        totalScoreDiv.classList.add('totalScore-div');
         totalScoreDiv.innerHTML = `<strong>Total Score:</strong> ${totalScore}`;
     
         // Append the data elements to the row container
@@ -30,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     async function getScoreCard() {
         try {
-            const response = await fetch('/api/test');
+            const response = await fetch('http://localhost:3434/api/test');
             if (!response.ok) {
                 throw new Error('API request failed');
             }
@@ -39,10 +63,15 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('Type of scorecardArray:', typeof scorecardArray);
             scorecardArray.forEach(({ Position, Name, Rank, TotalScore }) => {
                 makeListItem(Position, Name, Rank, TotalScore);
+                count++; 
             });
-        } catch (error) {
-            console.error('Error:', error);
-        }
+         } catch (error) {
+                console.error('Error fetching scorecard:', error);
+                if (error instanceof TypeError) {
+                    console.error('There was a network error:', error.message);
+                    // Additional logic to handle network errors can go here
+                }
+            }
     }
 });
 
