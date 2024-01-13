f6a1114e7adcbedb7f471b4a189fd5f0cf7d8353
Tanner <tannerhlyon@gmail.com>
2024-01-13 12:48:59 -0800

    fixed injection into current website issue

--- extension/src/tournament-details/tournamentDetailsPopup.ts
+++ extension/src/tournament-details/tournamentDetailsPopup.ts
@@ -1,71 +1,86 @@
-// import { loadContent, PageName } from './contentLoader';
+import { loadContent, PageName } from '../contentLoader';
 
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     getTournamentDetails(); 
+document.addEventListener('DOMContentLoaded', () => {
+    getTournamentDetails(); 
 
-//     document.getElementById('tournamentDetailsLink')?.addEventListener('click', () => {
-//         loadContent('tournament-details');
-//     });
-//     document.getElementById('courseDetailsLink')?.addEventListener('click', () => {
-//         loadContent('course-details');
-//     });
-//     document.getElementById('loginLink')?.addEventListener('click', () => {
-//         loadContent('login');
-//     });
-//     document.getElementById('settingsLink')?.addEventListener('click', () => {
-//         loadContent('settings');
-//     });
+    document.getElementById('tournamentDetailsLink')?.addEventListener('click', () => {
+        loadContent('tournament-details');
+    });
+    document.getElementById('courseDetailsLink')?.addEventListener('click', () => {
+        loadContent('course-details');
+    });
+    document.getElementById('loginLink')?.addEventListener('click', () => {
+        loadContent('login');
+    });
+    document.getElementById('settingsLink')?.addEventListener('click', () => {
+        loadContent('settings');
+    });
+    let count = 0; 
 
+    const makeListItem = (title: string, element: string | number) => {
 
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('data-row'); // Use the 'data-row' class for styling
 
-//     async function getTournamentDetails() {
-//         try {
-//             const response = await fetch('http://localhost:3434/api/test');
-//             if (!response.ok) {
-//                 throw new Error('API request failed');
-//             }
-//             const tournamentObj = await response.json();
-//             console.log('Scorecard data:', tournamentObj);
-//             console.log('Type of scorecardArray:', typeof tournamentObj);
-//             tournamentObj.forEach(({ Position, Name, Rank, TotalScore }: {
-//                 Position: number;
-//                 Name: string;
-//                 Rank: string;
-//                 TotalScore: number;
-//             }) => {
-//                 makeListItem(Position, Name, Rank, TotalScore);
-//                 count++;
-//             });
-//          } catch (error) {
-//                 console.error('Error fetching scorecard:', error);
-//                 if (error instanceof TypeError) {
-//                     console.error('There was a network error:', error.message);
-//                     // Additional logic to handle network errors can go here
-//                 }
-//             }
-//     }
-//     const menuIcon = document.getElementById('menuIcon');
-//     menuIcon?.addEventListener('click', toggleMenu);
+        const divs = document.createElement('div');
+        divs.classList.add(`${element}-div`);
+        divs.innerHTML = `<strong>${title}:</strong> ${element}`;
 
-//     document.addEventListener('click', closeMenu);
-// }); 
+        if (count % 2 !== 0) {
+            divs.style.backgroundColor = '#309C64'; // Change to your specific green color
+            // rowContainer.style.color = '#FAFAF1'
+        } else {
+            divs.style.backgroundColor = '#D4D4D2'; // Change to your specific gray color
+        }
+
+        rowContainer.appendChild(divs)
+    };
 
 
-// function toggleMenu() {
-//     const menu = document.getElementById('popupMenu');
-//     if (menu) {
-//         menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-//     }
-// }
+
+    async function getTournamentDetails() {
+        try {
+            const response = await fetch('http://localhost:3434/api/tournament');
+            if (!response.ok) {
+                throw new Error('API request failed');
+            }
+            const tournamentObj = await response.json();
+            console.log('Tournament Data:', tournamentObj); 
+            for(let el of tournamentObj){
+                makeListItem(tournamentObj, tournamentObj[el]);
+                count++;  
+            }
+            
+         } catch (error) {
+                console.error('Error fetching scorecard:', error);
+                if (error instanceof TypeError) {
+                    console.error('There was a network error:', error.message);
+                    // Additional logic to handle network errors can go here
+                }
+            }
+    }
+    const menuIcon = document.getElementById('menuIcon');
+    menuIcon?.addEventListener('click', toggleMenu);
+
+    document.addEventListener('click', closeMenu);
+}); 
+
+
+function toggleMenu() {
+    const menu = document.getElementById('popupMenu');
+    if (menu) {
+        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    }
+}
     
-//     // const menu = document.getElementById('popupMenu');
-//     // menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    // const menu = document.getElementById('popupMenu');
+    // menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 
 
-// function closeMenu(event: MouseEvent) {
-//     const menu = document.getElementById('popupMenu');
-//     if (menu && menu.style.display === 'block' && !menu.contains(event.target as Node) && !document.getElementById('menuIcon')?.contains(event.target as Node)) {
-//         menu.style.display = 'none';
-//     }
-// };
+function closeMenu(event: MouseEvent) {
+    const menu = document.getElementById('popupMenu');
+    if (menu && menu.style.display === 'block' && !menu.contains(event.target as Node) && !document.getElementById('menuIcon')?.contains(event.target as Node)) {
+        menu.style.display = 'none';
+    }
+};
